54f41e8d1a95ebbba64a7639978f8eee0b10b41b
TarunRathore12 <rathore24tarun@gmail.com>
2024-02-20 00:34:39 +0530

    Created get & post comments api in accordance with posts id

---
+++ comments/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+
+const app = express();
+
+// The below statement parses each incoming request & make the req body accessible to backend
+app.use(bodyParser.json());
+
+// Initially storing data on run time
+const commentsByPostsId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostsId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+  // Using crypto in order to associate each post with a random ID
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
+  console.log("checking req body", req.body);
+  const comments = commentsByPostsId[req.params.id] || [];
+  comments.push({
+    commentId, content
+  });
+  commentsByPostsId[req.params.id] = comments;
+
+  res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+  console.log("listening on 4001 for COMMENTS Service !!!");
+});
